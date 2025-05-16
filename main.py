d522428aabeaf5561a8ff5b37535dafbc1d67a77
Kauan Rodrigues <kaurodb@gmail.com>
2025-05-16 00:46:14 -0300

    [feat] ✨ : Célula 11

--- main.py
+++ main.py
@@ -59,3 +59,7 @@ def model_fit(inputs, target, w, b, epochs=200, lr=0.001):
         if (epoch + 1) % (epochs / 10) == 0:
             print(f'Epoch: [{(epoch+1)}/{epochs}] Loss: [{loss:.6f}]')
     return w, b
+
+#Célula 11
+x = np.arange(1, 20, 2)
+Y = get_linear_curve(x, 0.18, 0, ruido=0.2)
